293edac6e17b01fff5be1ac639e88362a00937ea
MatheusHenriqueDevz <matheus.silva611@fatec.sp.gov.br>
2025-12-11 21:31:13 -0300

    chore: initial project setup from spring initializr

---
+++ src/main/java/com/titanrun/titan_run_api/TitanRunApiApplication.java
@@ -0,0 +1,13 @@
+package com.titanrun.titan_run_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitanRunApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TitanRunApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/titanrun/titan_run_api/TitanRunApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.titanrun.titan_run_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TitanRunApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
